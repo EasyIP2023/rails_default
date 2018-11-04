ccd74c079523e025f069a6a65ac6bbac3d043e16
EasyIP2023 <vdavis@siu.edu>
2018-11-04 15:11:21 -0600

    add better verification in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :lockable, :timeoutable, :trackable
 
+  # Email regex
+  validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validate :phone_length
   validates_presence_of :phone
 
   def has_role?(role_sym)
     roles.any? { |r| r.name.underscore.to_sym == role_sym }
   end
+
+  private
+
+    def phone_length
+      errors.add(:phone, 'Phone # length must be 10 ex. 9998887777') if self.phone.length != 10
+    end
 
 end
